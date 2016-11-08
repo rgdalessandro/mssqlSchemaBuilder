7a1ad225ef824c38e69ed749ba9d830d6113e52c
Ricardo D'Alessandro <rgda.geo@yahoo.com>
2016-11-07 16:26:56 -0800

    changed folder structure

--- schema_builder.js
+++ schema_builder.js
@@ -4,37 +4,62 @@ var sql = require('seriate');
 var dbConfig = require('./secrets.js').dbConfig;
 sql.setDefaultConfig( dbConfig );
 
-var types = './types';
-var pre_tables = './pre_tables';
-var tables = './tables';
+var types = './typ';
+var tables = './tab';
+// preTables are the tables that need to be crated first because they will
+// later be referenced in subsequent scripts for indexing
+var preTables = [ 
+	'dbo.ClaimCodeCategories.Table.sql',
+	'dbo.GhostDraftDocuments.Table.sql',
+	'dbo.GhostDraftTemplatePackages.Table.sql',
+	'dbo.GhostDraftTemplates.Table.sql',
+	'dbo.Invoice.Table.sql',
+	'dbo.Payment.Table.sql',
+	'dbo.Policy.Table.sql',
+	'dbo.State.Table.sql',
+	'dbo.Users.Table.sql'
+]
 
-// First, run SQL to define all column types
+// First, run SQL to define all custom data types
+console.log('\nGenerating custom data types...');
 generateFileNames(types);
 
-// Second, generate tables that will later be referenced for indexing
+// Second, generate preTables that will later be referenced for indexing
 setTimeout(function(){
-	generateFileNames(pre_tables);
+	console.log('\nGenerating preTables...');
+    preTables.forEach(file => {
+	    runSQL(tables + '/' + file);
+	});
 }, 1000);
 
 // Third, generate all remaining tables
 setTimeout(function(){
+	console.log('\nGenerating remaining tables...');
 	generateFileNames(tables);
 }, 3000);
 
+setTimeout(function(){
+	console.log('\nAll done!\n');
+}, 5000);
 
-// This function creates an array of all filenames within a folder
+// This function creates an array of all table filenames within the tab folder
+// and removes the preTables that were already generated
 function generateFileNames(directoryPath) {
 	fs.readdir(directoryPath, (err, files) => {
-	  files.forEach(file => {
-	    runSQL(directoryPath + '/' + file);
-	  });
+	    files.filter(function(table) {
+	    	return preTables.indexOf(table) === -1;
+	    })
+	    .forEach(file => {
+		    runSQL(directoryPath + '/' + file);
+		});
 	});
 }
 
 // This function runs the indicated *.sql file
 // The *.sql file needed to be cleaned up due to its encoding and use of keyword 'GO'
 function runSQL(filePath) {
-	var startLine = filePath.slice(0,7) === './types' ? 3 : 7;
+	console.log(filePath);
+	var startLine = filePath.slice(0,5) === './typ' ? 3 : 7;
 	fs.readFile(filePath, 'ucs2', function(err, data) {
 		return sql.execute({
 			query: data.slice(1).split('\n').slice(startLine).join('\n').split('GO').join('\n')
